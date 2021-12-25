ab3cc45732faed0975490f4fb13e24b206ee31b2
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2021-12-25 12:08:18 +0800

    center the NCards

--- src/pages/Name.vue
+++ src/pages/Name.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="fixed w-full h-full bg-dark-700">
-    <div class="flex justify-center mt-[25vh]">
-      <NameCard></NameCard>
-    </div>
+  <div class="fixed w-full h-full bg-dark-700 flex justify-center items-center">
+    <NameCard class="m-auto"></NameCard>
   </div>
 </template>
--- src/pages/Wish.vue
+++ src/pages/Wish.vue
@@ -3,17 +3,16 @@ import { useRoute } from 'vue-router'
 import { cipher } from '../utils/cipher'
 
 const route = useRoute()
-const cipheredName = route.params.name
+const routeParams = route.params
+const cipheredName = routeParams.name
 const name = cipher(cipheredName.toString(), true)
 const title = `Happy CNY from ${name}!`
 </script>
 
 <template>
-  <div class="fixed w-full h-full bg-dark-700">
-    <div class="flex justify-center mt-10">
-      <n-card :title="title" class="max-w-[90vw] h-[133px]">
-        Happy CNY
-      </n-card>
-    </div>
+  <div class="fixed w-full h-full bg-dark-700 flex justify-center items-center">
+    <n-card :title="title" class="max-w-[600px] w-[85vw]">
+      Happy CNY
+    </n-card>
   </div>
 </template>
